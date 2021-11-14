0922bd595e074d67f15ce8e3d92fd0b28e40227a
Sean Raborg <sean.raborg@gmail.com>
2021-11-14 16:13:45 -0700

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springramework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springramework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springramework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springramework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
