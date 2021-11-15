3ac0a1153c77674761c9ca456a93e880447c09a2
Sean Raborg <sean.raborg@gmail.com>
2021-11-15 01:01:47 -0700

    Implemented Map Based Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/springramework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springramework.sfgpetclinic.services.map;
+
+import guru.springramework.sfgpetclinic.model.Pet;
+import guru.springramework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Pet owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Pet save(Pet owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springramework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springramework.sfgpetclinic.services.map;
+
+import guru.springramework.sfgpetclinic.model.Vet;
+import guru.springramework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Vet save(Vet owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
