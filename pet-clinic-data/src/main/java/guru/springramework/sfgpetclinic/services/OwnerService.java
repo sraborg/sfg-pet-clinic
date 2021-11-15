8993a8793e675109c4b38529f9cd69877f8628aa
Sean Raborg <sean.raborg@gmail.com>
2021-11-14 19:34:47 -0700

    Created Interfaces for Services. Closes #4.

---
+++ pet-clinic-data/src/main/java/guru/springramework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package guru.springramework.sfgpetclinic.services;
+
+import guru.springramework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
