0a639c71595086965116249f9e92cbb68033b3d6
Sean Raborg <sean.raborg@gmail.com>
2021-11-15 02:04:18 -0700

    Adding Owners page and controller. Closes #13. - Fixed typos minor typos

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+
     @RequestMapping({"", "/", "/index","/index.html"})
     public String listOwners() {
 
