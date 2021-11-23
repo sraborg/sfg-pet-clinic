1faef38ec7b86a40aba20976618e2006e1fa8e58
Sean Raborg <sean.raborg@gmail.com>
2021-11-23 16:10:58 -0700

    Implemented JPA Repositories. Closes #39.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public interface SpecialtyRepository extends CrudService<Speciality, Long> {
+
+}
