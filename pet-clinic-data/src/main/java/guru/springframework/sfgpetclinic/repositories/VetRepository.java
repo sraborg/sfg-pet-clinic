eeb47c8e461d02b4c7290babdaa6e5ca06fe1f62
Sean Raborg <sean.raborg@gmail.com>
2021-11-23 16:55:17 -0700

    Added Spring Data JPA service for vets. Closes #41.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -1,7 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Vet;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
 
-public interface VetRepository extends CrudService<Vet, Long> {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
